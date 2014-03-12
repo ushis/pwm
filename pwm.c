cf1c4a2db81f9ab11588476297b25553e29b19c0
ushi <ushi@honkgong.info>
2014-03-12 11:37:40 +0100

    Added help and version commands.

--- pwm.c
+++ pwm.c
@@ -10,19 +10,25 @@
 
 const char *usage_str = "pwm <cmd> [<args>]";
 
+void usage();
+void version();
+
 typedef struct {
   char *cmd;
   char *bin;
+  void (*func)();
   char *help;
 } cmd_t;
 
 const cmd_t cmds[] = {
-  {"del",  PWM_EXEC_DIR"/pwm-del",  "delete a password"},
-  {"gen",  PWM_EXEC_DIR"/pwm-gen",  "generate a password"},
-  {"get",  PWM_EXEC_DIR"/pwm-get",  "retrieve a password"},
-  {"list", PWM_EXEC_DIR"/pwm-list", "list all passwords"},
-  {"log",  PWM_EXEC_DIR"/pwm-log",  "print the log"},
-  {"set",  PWM_EXEC_DIR"/pwm-set",  "set a password"}
+  {"del",     PWM_EXEC_DIR"/pwm-del",  NULL,    "delete a password"},
+  {"gen",     PWM_EXEC_DIR"/pwm-gen",  NULL,    "generate a password"},
+  {"get",     PWM_EXEC_DIR"/pwm-get",  NULL,    "retrieve a password"},
+  {"help",    NULL,                    usage,   "show this help"},
+  {"list",    PWM_EXEC_DIR"/pwm-list", NULL,    "list all passwords"},
+  {"log",     PWM_EXEC_DIR"/pwm-log",  NULL,    "print the log"},
+  {"set",     PWM_EXEC_DIR"/pwm-set",  NULL,    "set a password"},
+  {"version", NULL,                    version, "show version"}
 };
 
 void
@@ -34,7 +40,11 @@ usage() {
   for (i = 0; i < (sizeof(cmds)/sizeof(cmd_t)); i++) {
     fprintf(stderr, "  %-14s  %s\n", cmds[i].cmd, cmds[i].help);
   }
-  exit(EXIT_FAILURE);
+}
+
+void
+version() {
+  fprintf(stderr, "pwm %s\n", PWM_VERSION);
 }
 
 const cmd_t *
@@ -74,10 +84,17 @@ main(int argc, char **argv) {
 
   if (argc < 2) {
     usage();
+    exit(EXIT_FAILURE);
   }
 
   if ((cmd = cmd_find(argv[1])) == NULL) {
     usage();
+    exit(EXIT_FAILURE);
   }
-  exit(cmd_exec(cmd, &argv[1]));
+
+  if (cmd->bin != NULL) {
+    exit(cmd_exec(cmd, &argv[1]));
+  }
+  cmd->func();
+  exit(EXIT_SUCCESS);
 }
