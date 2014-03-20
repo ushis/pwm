2c17d00da59e16675d0530e60265791a06015799
ushi <ushi@honkgong.info>
2014-03-20 15:47:44 +0100

    [test] Updated .travis.sh to build and test pwm

--- .travis.sh
+++ .travis.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env sh
 
-cd ~
-git clone https://github.com/libgit2/libgit2.git libgit2
+if test "${1}" = "libgit2"; then
+  cd ~
+  git clone https://github.com/libgit2/libgit2.git libgit2
 
-cd libgit2
-git checkout v0.20.0
+  cd libgit2
+  git checkout v0.20.0
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DTHREADSAFE=ON
-make && sudo make install
+  cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr -DTHREADSAFE=ON
+  make
+  sudo make install
+  exit $?
+fi
+
+if test "${1}" = "pwm"; then
+  autoconf -o configure configure.ac
+  ./configure
+  make && make test
+  exit $?
+fi
+
+echo "unknown command: ${1}"
+exit 1
